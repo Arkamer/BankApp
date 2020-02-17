7deb50f5b0543d3a3f602ccfe8a4cb2023e736d0
Ailton Lessa <ailton.lessa@ufpe.br>
2020-02-17 00:03:29 -0300

    First commit. Created views and LoginViewController outlets

---
+++ BankAppTest/View Controllers/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  BankAppTest
+//
+//  Created by Ailton Lessa on 16/02/20.
+//  Copyright © 2020 Ailton. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+}
---
+++ BankAppTest/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BankAppTest
+//
+//  Created by Ailton Lessa on 16/02/20.
+//  Copyright © 2020 Ailton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
